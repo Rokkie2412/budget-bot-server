4fd75a29397777ef8455f5a0c4acbda342399e0a
Ervan Jovian <ervanjovian@gmail.com>
2026-03-28 13:04:42 +0700

    next create command .help

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,12 @@ export const TRANSACTION_TYPE = {
 } as const
 
 export const ENC_ALGORITHM = 'aes-256-cbc' as const;
+
+export const HELP_COMMANDS = [
+  { command: 'masuk [nominal] [ket]', desc: 'Catat pemasukan' },
+  { command: '[nomimal] [ket]', desc: 'Catat pengeluaran' },
+  { command: '[ket] [nomimal]', desc: 'Catat pengeluaran' },
+  { command: '.rekap', desc: 'Lihat laporan bulan ini' },
+  { command: '.cek [angka]', desc: 'Lihat histori transaksi' },
+  // { command: '.batal', desc: 'Hapus transaksi terakhir' },
+];
