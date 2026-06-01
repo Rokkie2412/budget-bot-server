3db0c7d40ccd1d9649602c2d9e6f2d61583406a0
Ervan Jovian <ervanjovian@gmail.com>
2026-06-01 19:46:52 +0700

    add env

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,70 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import mongoose from 'mongoose';
-import qrcode from 'qrcode-terminal';
-import { Client, LocalAuth } from 'whatsapp-web.js';
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import qrcode from "qrcode-terminal";
+import { Client, LocalAuth } from "whatsapp-web.js";
 
-import { Transaction, UserConnected } from './models';
+import { Transaction, UserConnected } from "./models";
 import {
   deleteLastTransaction,
   hashUserId,
   transactionHistoryBy,
   TransactionInMatchWithRegex,
   TransactionOutMatchWithRegex,
-  transactionRecordCurrentMonth
-} from './utils';
-import { helpCommand } from './utils/help';
+  transactionRecordCurrentMonth,
+} from "./utils";
+import { helpCommand } from "./utils/help";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development";
 const dbURI = isDev ? process.env.MONGO_URI_DEV : process.env.MONGO_URI_PROD;
 
 //connect to mongodb
-mongoose.connect(dbURI || '')
-  .then(() => console.log(`Connected to MongoDB ${isDev ? 'development' : 'production'} mode 🛻`))
+mongoose
+  .connect(dbURI || "")
+  .then(() =>
+    console.log(
+      `Connected to MongoDB ${isDev ? "development" : "production"} mode 🛻`,
+    ),
+  )
   .catch((err) => {
-    console.error('❌ Failed to connect to MongoDB', err)
+    console.error("❌ Failed to connect to MongoDB", err);
     return process.exit(1);
   });
 
 //setup whatsapp client
 const client = new Client({
   authStrategy: new LocalAuth(),
-  puppeteer: { 
-    handleSIGINT: false, 
+  puppeteer: {
+    handleSIGINT: false,
     args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      // '--single-process',
-      '--disable-gpu'
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-gpu",
     ],
-  }
-})
+  },
+});
 
-client.on('qr', (qr) => {
-  console.log('QR code received, scanning...');
+client.on("qr", (qr) => {
+  console.log("QR code received, scanning...");
   qrcode.generate(qr, { small: true });
-})
+});
 
-client.on('ready', (): void => {
-  console.log('✅ WhatsApp client is ready!');
-})
+client
+  .on("ready", (): void => {
+    console.log("✅ WhatsApp client is ready!");
+  })
 
   //core logic of client
-  .on('message', async (message) => {
+  .on("message", async (message) => {
     const contact = await message.getContact();
     const rawNumber = contact.number;
     const formattedNumber = `+${rawNumber}`;
@@ -70,35 +76,34 @@ client.on('ready', (): void => {
 
     try {
       const checkConnectedUser = await UserConnected.findOne({
-        userId: formattedNumber
+        userId: formattedNumber,
       });
 
       if (!checkConnectedUser) {
-        console.log('User tidak terdaftar', checkConnectedUser);
+        console.log("User tidak terdaftar", checkConnectedUser);
         return;
       }
 
       const hashedUserId = hashUserId(formattedNumber);
       let match;
 
-      if (match = message.body.match(rekapRegex)) {
+      if ((match = message.body.match(rekapRegex))) {
         await transactionRecordCurrentMonth(hashedUserId, message, Transaction);
-      } else if (match = message.body.match(incomeRegex)) {
+      } else if ((match = message.body.match(incomeRegex))) {
         await TransactionInMatchWithRegex(hashedUserId, message, match);
-      } else if (match = message.body.match(historyRegex)) {
+      } else if ((match = message.body.match(historyRegex))) {
         transactionHistoryBy(hashedUserId, message, match);
-      } else if (match = message.body.match(helpRegex)) {
+      } else if ((match = message.body.match(helpRegex))) {
         helpCommand(message, match);
-      } else if (match = message.body.match(undoRegex)) {
+      } else if ((match = message.body.match(undoRegex))) {
         deleteLastTransaction(hashedUserId, message);
       } else {
         await TransactionOutMatchWithRegex(hashedUserId, message);
       }
-
     } catch (error) {
-      console.error('❌ Error:', error);
+      console.error("❌ Error:", error);
     }
-  })
+  });
 
 client.initialize();
 
